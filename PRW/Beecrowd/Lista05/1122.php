1673240203e6230baf224d9ddc6c9aa1932a2abe
Cauã Salvador Lima <119822575+Csaalvador@users.noreply.github.com>
2023-03-23 16:40:28 -0300

    Exercícios

--- PRW/Beecrowd/Lista05/1122.php
+++ PRW/Beecrowd/Lista05/1122.php
@@ -1,70 +1,12 @@
 <?php
+///A primeira linha de um caso de teste contém três inteiros N (2 ≤ N ≤ 1000), L (1 ≤ L ≤ 30 ) e C (1 ≤ C ≤ 70) 
 
-/*int N, L, C;
-    char story[100002];
-    int len, chars, lines, pages, checkpoint;
-    int i, j;
+$x = explode(" ", fgets(STDIN));
 
-    while (scanf("%d%d%d", &N, &L, &C) != EOF) {
-        getchar();
-
-        fgets(story, 100002, stdin);
-        len = strlen(story) - 1;
-
-        chars = checkpoint = 0;
-        lines = pages = 1;
-
-        for (i = 0; i < len; ++i) {
-            if (i && story[i-1] == ' ')
-                checkpoint = i;
-
-            if (chars == C) {
-                ++lines;
-                if (story[i] == ' ')
-                    chars = 0;
-                else
-                    chars = i - checkpoint + 1;
-            } else
-                ++chars;
-        }
-
-        pages = lines / L + (lines % L > 0);
-
-        printf("%d\n", pages);
-    }
-
-    return 0;
-}
-*/
-
-$N = (int) fgets(STDIN);
-$L = (int) fgets(STDIN);
-$C = (int) fgets(STDIN);
-$story = fgets(STDIN);
-
-$len = strlen($story) - 1;
-$chars = $checkpoint = 0;
-$lines = $pages = 1;
-
-for ($i = 0; $i < $len; $i++) {
-    if ($i && $story[$i-1] == ' ')
-        $checkpoint = $i;
-
-    if ($chars == $C) {
-        $lines++;
-        if ($story[$i] == ' ')
-            $chars = 0;
-        else
-            $chars = $i - $checkpoint + 1;
-    } else
-        $chars++;
-}
-
-$pages = $lines / $L + ($lines % $L > 0);
-
-echo $pages;
+$N = trim($x[0]); // o número de palavras do conto de Machado 
+$L = trim($x[1]);// o número máximo de linhas que o conto pode ter
+$C = trim($x[2]);// o número máximo de caracteres que o conto pode ter por linha
 
 
 
 
-?>
