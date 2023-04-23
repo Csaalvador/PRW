48c75dcc9dc004dede8d85036dff67dc1d679cdc
Csaalvador <caua.lima@aluno.ifsp.edu.br>
2023-04-22 22:35:18 -0300

    if

---
+++ PRW/Formularios/Lista01/Ex01/01.php
@@ -0,0 +1,15 @@
+<?php
+
+$somar = $_POST['somar'];
+
+
+if($somar){
+    echo "<h2>Resultado: teste</h2>";
+}
+        
+    else{
+        echo "<h2>Resultado: teste</h2>";
+    }
+
+?>
+<a href='01.php'><button>Voltar</button></a>
