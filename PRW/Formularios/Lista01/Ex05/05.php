32b92dd5c2c04c9ca60fcd1f62bf474eae4bdef1
Csaalvador <caua.lima@aluno.ifsp.edu.br>
2023-04-26 17:38:06 -0300

    IIFSP

--- PRW/Formularios/Lista01/Ex05/05.php
+++ PRW/Formularios/Lista01/Ex05/05.php
@@ -1,10 +1,44 @@
+<fieldset>
 
-<?php
-$n1 = $_GET["n1"];
-$n2 = $_GET["n2"];
+    <?php
+   
+    $n1 = $_GET["n1"];
+    $n2 = $_GET["n2"];
 
-$area = $n1 * $n2;
+    $area = $n1 * $n2;
 
-echo "A área do retângulo é: $area";
+    echo "A área do retângulo é:<mark>$area</mark>";
+    echo "<br>";
 
-?>
+    ?>
+    <style>
+        button {
+            background-color: black;
+            color: white;
+            border: 2px solid white;
+            border-radius: 5px;
+            padding: 6px 10px;
+        }
+
+        button:hover {
+            background-color: white;
+            color: white;
+            border: 2px solid white;
+        }
+
+        body {
+            color: white;
+            background-color: black;
+            left: 50%;
+            top: 50%;
+            position: absolute;
+            transform: translate(-50%, -50%);
+            font-size: 20px;
+
+        }
+    </style>
+    <br>
+    <a href="05.html"><button>Voltar</button></a>
+
+
+</fieldset>
